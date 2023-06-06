1aff0786f805046eb898d86a41097bfd0781405c
LeviHHH <LeviHHH@protonmail.com>
2023-06-06 18:50:16 +0800

    multi transaction test

---
+++ scripts/MultiRawSigner.ts
@@ -0,0 +1,51 @@
+import {
+    JsonRpcProvider,
+    PubkeyWeightPair, PublicKey,
+    SerializedSignature,
+    SignerWithProvider,
+    SuiAddress,
+    toMultiSigAddress,
+    combinePartialSigs
+} from "@mysten/sui.js";
+import {blake2b} from "@noble/hashes/blake2b";
+
+export class MultiRawSigner extends SignerWithProvider {
+    public readonly pksWeightPairs: PubkeyWeightPair[];
+    public readonly threshold: number;
+
+    constructor(pks: PubkeyWeightPair[], threshold: number, provider: JsonRpcProvider) {
+        super(provider);
+        this.pksWeightPairs = pks;
+        this.threshold = threshold;
+    }
+
+    async getAddress(): Promise<SuiAddress> {
+        return toMultiSigAddress(this.pksWeightPairs, this.threshold);
+    }
+
+    async signData(data: Uint8Array): Promise<SerializedSignature> {
+        throw "don't support";
+    }
+
+    connect(provider: JsonRpcProvider): MultiRawSigner {
+        return new MultiRawSigner(this.pksWeightPairs, this.threshold, provider);
+    }
+
+    combinePartialSigs(
+        sigs: SerializedSignature[],
+    ): SerializedSignature {
+        return combinePartialSigs(sigs, this.pksWeightPairs, this.threshold);
+    }
+
+    static new(pubkeys: PublicKey[], weights: number[], threshold: number, provider: JsonRpcProvider): MultiRawSigner {
+        if (pubkeys.length != weights.length) throw "pubkeys and weigths mismatch";
+        const weightSum = weights.reduce((sum, weight) => sum + weight);
+        if (weightSum < threshold) "weightSum < threshold";
+        if (threshold > 65535) throw "threshold too big";
+        const pks = pubkeys.map((pubkey, index) => ({
+            pubKey: pubkey,
+            weight: weights[index],
+        }));
+        return new MultiRawSigner(pks, threshold, provider);
+    }
+}
---
+++ scripts/multi_transaction.ts
@@ -0,0 +1,77 @@
+import {
+    Ed25519Keypair,
+    JsonRpcProvider,
+    RawSigner,
+    TransactionBlock,
+    localnetConnection
+} from "@mysten/sui.js";
+import { blake2b } from "@noble/hashes/blake2b";
+import {MultiRawSigner} from "./MultiRawSigner";
+
+const default_provider = new JsonRpcProvider(localnetConnection);
+
+async function faucet(to: string, provider: JsonRpcProvider = default_provider) {
+    to = to.slice(2);
+    console.log("----use faucet:", provider.connection.faucet);
+    console.log("----faucet to", to);
+    await provider.requestSuiFromFaucet(to);
+    const coins = await provider.getAllBalances({owner: to});
+    console.log("coins:", coins.map(coin => coin));
+}
+
+function createMultiAccount(ownerCount: number, threshold: number, provider: JsonRpcProvider = default_provider) {
+    const keypairs = Array(ownerCount).fill(0).map(() => new Ed25519Keypair());
+    const pks = keypairs.map(keypair=>keypair.getPublicKey());
+    const weights = pks.map((_,i)=>i);
+    const owners = keypairs.map(keypair=> new RawSigner(keypair, provider));
+    return {
+        multiSigner: MultiRawSigner.new(pks, weights, threshold, provider),
+        owners,
+    };
+}
+
+export function hashTypedData(typeTag: string, data: Uint8Array): Uint8Array {
+    const typeTagBytes = Array.from(`${typeTag}::`).map((e) => e.charCodeAt(0));
+
+    const dataWithTag = new Uint8Array(typeTagBytes.length + data.length);
+    dataWithTag.set(typeTagBytes);
+    dataWithTag.set(data, typeTagBytes.length);
+
+    return blake2b(dataWithTag, { dkLen: 32 });
+}
+
+const main = async () => {
+    const provider = default_provider;
+
+    const multiAccount = createMultiAccount(3, 2);
+    const sender = multiAccount.multiSigner;
+    const owners = multiAccount.owners;
+    const recipient = owners[0];
+
+    await faucet(await sender.getAddress());
+
+    const senderCoin = await provider.getCoins({ owner: await sender.getAddress() });
+    const coinObjectIds = senderCoin.data.map(coin=>coin.coinObjectId);
+
+    const tx = new TransactionBlock();
+    tx.transferObjects(coinObjectIds.slice(1).map(objId=>tx.object(objId)), tx.pure(await recipient.getAddress()));
+    tx.setSender(await sender.getAddress());
+    const txBytes = await tx.build({ provider });
+    const ownerSigs = await Promise.all(owners.map(owner=>owner.signTransactionBlock({ transactionBlock: txBytes })));
+    const senderSig = sender.combinePartialSigs(ownerSigs.map(ownerSig=>ownerSig.signature));
+
+    const result = await provider.executeTransactionBlock({
+        transactionBlock: txBytes,
+        signature: [senderSig],
+        options: {
+            showEffects: false,
+            showBalanceChanges: true,
+            showEvents: false,
+            showInput: false,
+            showObjectChanges: false,
+        },
+    });
+    console.log(result);
+};
+
+main().then();
